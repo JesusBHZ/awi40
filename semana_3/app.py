b5023f06f2c0efa825b46fd16990c3a677fbe20f
JesusBHZ <1721110061@utectulancingo.edu.mx>
2023-01-26 22:03:26 -0600

    Cookies

--- semana_3/app.py
+++ semana_3/app.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 urls = (
-    '/', 'CookieSet'
+    '/(.*)', 'CookieSet'
     )
 
 render = web.template.render('templates',base='layout')
@@ -11,48 +11,41 @@ app = web.application(urls, globals())
 
 
 class CookieSet:
-    def GET(self):
+    def GET(self, user):
         try:
             cookie = web.cookies()
-            
-            user = "Jesus"
+            visitas = "0"
+            print(cookie)
             
             fetch = datetime.today().strftime('%Y-%m-%d')
             hora = datetime.today().strftime('%H:%M')
-
-
-            web.setcookie('usuario',user, expires="",domain=None)
+            web.setcookie('userr',user, expires="",domain=None)
             web.setcookie('fechaVisita',fetch, expires="",domain=None)
             web.setcookie('horaVisita',hora, expires="",domain=None)
-        
+            
+            if user:
+                web.setcookie("nombre",user,expires="",domain=None)
+            else:
+                user = "Anonimo"
+                web.setcookie("nombre",user,expires="",domain=None)                
+
             if cookie.get("visitas"):
                 visitas = int(cookie.get("visitas"))
                 visitas +=1
                 web.setcookie("visitas", str(visitas), expires="",domain=None)
-                return "visitas "+str(visitas)
+
             else:
                 web.setcookie("visitas", str(1), expires="", domain=None)
-                return "Visitas " + "1"
+                visitas = "1"
+                
+            return "Visitas: " + str(visitas) + " Nombre: " + user + " Hora: " + hora + " Fecha: " + fetch
+                            
         except Exception as e:
             return "Error" + str(e.args)
+        
+        
     
 
 if __name__ == "__main__":
     #web.config.debug = False
     app.run()
-    
-
-"""
-         # No. de visitas, nombre de usuario (si lo pasa por url) si no guarda el nombre "anónimo", fecha de la visita, hora de la visita.
-        web.setcookie('No.visitas', 0, 3600)
-        user = "Jesus"
-        fetch = datetime.today().strftime('%Y-%m-%d')
-        hora = datetime.today().strftime('%H:%M')
-
-
-        web.setcookie('usuario',user, 3600)
-        web.setcookie('fechaVisita',fetch, 3600)
-        web.setcookie('horaVisita',hora, 3600)
-        
-        return "Your age is: " + web.cookies().age + "fecha: "+ web.cookies().fechaVisita + " hora "+web.cookies().horaVisita
-"""
